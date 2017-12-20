ea8acbf7c7ee7c563b61fc834805b3d4b2d2b282
Daniel Strolle <danielstrolle@gmail.com>
2017-12-20 14:04:04 -0600

    Update upload portfolio script to point to correct bucket

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,32 +3,17 @@ import StringIO
 import zipfile
 import mimetypes
 
+s3 = boto3.resource('s3')
 
-def lambda_handler(event, context):
-    sns = boto3.resource('sns')
-    topic = sns.Topic('arn:aws:sns:us-east-1:693833859909:deployPortfolioTopic')
+portfolio_bucket = s3.Bucket('portfolio.guru.danielstrolle.me')
+build_bucket = s3.Bucket('portfoliobuid.guru.danielstrolle.me')
 
-    try:
-        s3 = boto3.resource('s3')
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-        portfolio_bucket = s3.Bucket('portfolio.danielstrolle.me')
-        build_bucket = s3.Bucket('portfoliobuild.danielstrolle.me')
-
-        portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-
-        with zipfile.ZipFile(portfolio_zip) as myzip:
-            for nm in myzip.namelist():
-                obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
-        print "Job Done"
-        topic.publish(Subject='Portfolio Deployed', Message='Portfolio deployed successfully!')
-    except:
-        topic.publish(Subject='Portfolio Deploy Failed', Message='The portfolio was not deployed successfully')
-        raise
-
-    return 'Hello from Lambda'
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm,
+          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
