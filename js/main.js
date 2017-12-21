191e823a6d5531aecd54345c23684d5dc2ffc052
Daniel Strolle <danielstrolle@gmail.com>
2017-12-21 17:08:21 -0600

    Move static markup for work examples into React components

--- js/main.js
+++ js/main.js
@@ -1,5 +1,47 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work.js';
 
-console.log("Loaded react-dom");
-
-console.log("webpack works");
+const myWork = [
+  {
+    'title': "Weather Map Forecast",
+    'image': {
+      'desc': "A Single-Page Web App using Google's weather API",
+      'src': "images/ajax-api-ss.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Monkey Business LLC",
+    'image': {
+      'desc': "Single Page Web App using many AJAX calls",
+      'src': "images/movide_db-ss.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Dab-Lister",
+    'image': {
+      'desc': "A Java App mock-up of EBay with a twist, Dab-Lister",
+      'src': "images/adlister-ss.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Blog Mock-up",
+    'image': {
+      'desc': "Java App built using Spring MVC",
+      'src': "images/blog-ss.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Capstone Project",
+    'image': {
+      'desc': "Java App built using Spring MVC",
+      'src': "images/lmiml-ss.png",
+      'comment': ""
+    }
+  }
+]
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
